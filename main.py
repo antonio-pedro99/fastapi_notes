f778346566a971b839e94f0cfefae1d68e1e5f9d
Antonio Pedro <tonio.pedro99@gmail.com>
2022-03-22 02:48:35 +0530

    correct app name

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ async def shutdwon():
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port = 35814, reload = True)
+    uvicorn.run("main:app", host="127.0.0.1", port = 35814, reload = True)
